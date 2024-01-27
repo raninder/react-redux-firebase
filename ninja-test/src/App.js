d80b9f9ba3ceab262e670039bfa9a231e85a6160
EliteD <850248@pdsb.net>
2024-01-27 16:25:25 -0500

    project structure created

---
+++ ninja-test/src/App.js
@@ -0,0 +1,12 @@
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+function App() {
+  return (
+    <Router>
+    <div className="App">
+      <h1> Mario Plan</h1>
+    </div>
+    </Router>
+  );
+}
+
+export default App;
