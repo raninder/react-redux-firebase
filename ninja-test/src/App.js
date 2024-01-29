fd58a882ad9b02908162260c1d8fec92a4fcb440
EliteD <850248@pdsb.net>
2024-01-29 12:19:12 -0500

    Update App.js

--- ninja-test/src/App.js
+++ ninja-test/src/App.js
@@ -2,6 +2,9 @@ import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/dashboard/Dashboard'
 import ProjectDetails from './components/projects/ProjectDetails';
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Routes>
          <Route path="/" element={<Dashboard/>} />
          <Route path="/project/:id" element = {<ProjectDetails/>} />
+         <Route path="/signin" element={<SignIn/>} />
+         <Route path="/signup" element={<SignUp/>} />
+         <Route path="/create" element={<CreateProject/>} />
       </Routes>  
     </div>
     </Router>
