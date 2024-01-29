68560a977e3ef375dee4ede83f91afa93a5f1667
EliteD <850248@pdsb.net>
2024-01-29 12:19:03 -0500

    SignIn and SignUp

--- ninja-test/src/components/auth/SignUp.js
+++ ninja-test/src/components/auth/SignUp.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {useState} from 'react'
 
-function SignIn() {
+function SignUp() {
   
   const [state, setState] = useState({
     email: "",
-    password: ""
+    password: "",
+    firstName: "",
+    lastName: ""
 });
   const handleSubmit = (e) =>{
     e.preventDefault()
@@ -22,7 +24,7 @@ function SignIn() {
   return (
     <div className="container">
       <form onSubmit={handleSubmit} className="white">
-        <h5 className="grey-text text-darken-3">Sign In</h5>
+        <h5 className="grey-text text-darken-3">Sign Up</h5>
         <div className="input-field">
           <label htmlFor="email">Email:</label>
           <input type="email" id="email" onChange={handleChange}/>
@@ -34,7 +36,17 @@ function SignIn() {
         </div>
 
         <div className="input-field">
-          <button className='btn pink lighten-1 z-depth-0'>Login</button>
+          <label htmlFor="firstName">First Name:</label>
+          <input type="text" id="firstName" onChange={handleChange}/>
+        </div>
+
+        <div className="input-field">
+          <label htmlFor="lastName">Last Name:</label>
+          <input type="text" id="lastName" onChange={handleChange}/>
+        </div>
+
+        <div className="input-field">
+          <button className='btn pink lighten-1 z-depth-0'>Sign up</button>
          </div> 
       </form>
 
@@ -43,4 +55,4 @@ function SignIn() {
   )
 }
 
-export default SignIn
+export default SignUp
